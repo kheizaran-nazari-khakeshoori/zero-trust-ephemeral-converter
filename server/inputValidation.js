081f4782f86dc2611703ad00c08efb8abbcef1d4
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2026-09-05 20:09:29 +0200

    Allow the new output formats

--- server/inputValidation.js
+++ server/inputValidation.js
@@ -1,5 +1,5 @@
 export const MAX_UPLOAD_SIZE = 10 * 1024 * 1024;
-export const ALLOWED_TARGET_FORMATS = new Set(['html', 'csv']);
+export const ALLOWED_TARGET_FORMATS = new Set(['html', 'pdf', 'csv', 'webp']);
 
 export function validateTargetFormat(targetFormat) {
   return typeof targetFormat === 'string'
